c4a0c0b9d9741a141aa4ddcaa4dfb0407cee99a6
kunchamvasu <kuncham.vasu@landmarkgroup.in>
2019-09-24 09:11:19 +0530

    added landing page

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Book;
 
 class LandingController extends Controller
 {
@@ -11,7 +12,8 @@ class LandingController extends Controller
 
     public function index()
     {
-    	return view('landing', ['page' => '/']);
+    	$books = Book::all();
+    	return view('landing', ['page' => '/', 'books'=> $books]);
     }
 
 }
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,11 +5,14 @@
 @section('content')
 	<style type="text/css">
 		.landing {
-			margin-top: 200px;
-    		text-align: center;
+			margin-top: 20px;
 		}
 		.notify {
 			margin: 20px 0;
+		}
+		img {
+			width: 100%;
+			height: 100px;
 		}
 	</style>
 
@@ -24,6 +27,32 @@
 
 
 	<div class="landing">
-		<h3>welcome to Book store</h3>
+		@if(isset($books) && !empty($books))
+			<div class="row">
+				@foreach($books as $book)
+				  <div class="col-md-3" style="margin-bottom: 10px">
+					<div class="card">
+					  <img src="{{$book['image']}}" class="card-img-top">
+					  <div class="card-body">
+					    <h5 class="card-title">{{$book['name']}}</h5>
+					    <p class="card-text">{{$book['description']}}</p>
+					    By <span>{{$book['author']}}</span> <br>
+					    <button class="btn btn-primary addToCart">Add to cart</button>
+					  </div>
+					</div>
+				  </div>
+				@endforeach
+			</div>
+		@else
+			<h5 align="center">No books data found to display</h5>
+		@endif
 	</div>
+@endsection
+
+@section('JS')
+	<script type="text/javascript">
+		$('.addToCart').click(function(){
+			alert('this item is added to the cart');
+		})
+	</script>
 @endsection
